2899cb89400072a586742a7db570b67b9156d9b0
iamabin <wc2497535068@gmail.com>
2025-04-05 17:43:00 +1000

    push dockerfile

--- main.tf
+++ main.tf
@@ -1,25 +1,70 @@
+# terraform {
+#     required_providers {
+#         aws = {
+#             source  = "hashicorp/aws"
+#             version = "~> 4.0"
+#         }
+#     }
+# }
+#
+# provider "aws" {
+#     region = "us-east-1"
+#     shared_credentials_files = ["./credentials"]
+#     default_tags {
+#         tags = {
+#             Course       = "CSSE6400"
+#             Name         = "CoughOverflow"
+#             Automation   = "Terraform"
+#         }
+#     }
+# }
+#
+# resource "local_file" "url" {
+#     content  = "http://my-url/"  # Replace this string with a URL from your Terraform.
+#     filename = "./api.txt"
+# }
+
+
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 4.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
-    shared_credentials_files = ["./credentials"]
-    default_tags {
-        tags = {
-            Course       = "CSSE6400"
-            Name         = "CoughOverflow"
-            Automation   = "Terraform"
-        }
-    }
+  region                  = "us-east-1"
+  shared_credentials_files = ["./credentials"]
 }
 
-resource "local_file" "url" {
-    content  = "http://my-url/"  # Replace this string with a URL from your Terraform.
-    filename = "./api.txt"
+data "aws_ecr_authorization_token" "ecr_token" {}
+
+provider "docker" {
+  registry_auth {
+    address  = data.aws_ecr_authorization_token.ecr_token.proxy_endpoint
+    username = data.aws_ecr_authorization_token.ecr_token.user_name
+    password = data.aws_ecr_authorization_token.ecr_token.password
+  }
+}
+
+resource "aws_ecr_repository" "coughoverflow" {
+  name = "coughoverflow"
+}
+
+resource "docker_image" "coughoverflow" {
+  name = "${aws_ecr_repository.coughoverflow.repository_url}:latest"
+  build {
+    context = "."
+    dockerfile = "Dockerfile"
+  }
+}
+
+resource "docker_registry_image" "coughoverflow" {
+  name = docker_image.coughoverflow.name
 }
